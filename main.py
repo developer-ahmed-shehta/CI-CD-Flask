99ed7233ae3651a2ab545064b54599bb254d64f0
ahmedshehtaa <ahmedshehta0123@gmail.com>
2025-01-25 06:15:17 +0200

    fix deploy settings

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, CI/CD World!'
+    return render_template('index.html')
 
 @app.route('/goodbye')
 def goodbye_world():
     return 'Goodbye, CI/CD World!'
-
-@app.route('/goodnight')
-def goodnight():
-    return 'goodnight'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
